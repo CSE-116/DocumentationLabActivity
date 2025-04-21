8a8a7ac933fb3c78e3ee15bb7c90466c759b2a54
jbcarras <jbcarras@buffalo.edu>
2025-04-21 00:53:59 -0400

    Setup activity

--- src/ProblemSet.java
+++ src/ProblemSet.java
@@ -9,21 +9,6 @@ public class ProblemSet {
 
 
     public static String furthestNode(String filePath, String startNode) {
-
-        // Get edge list
-        ArrayList<Edge> edgeList = Utilities.edgeListFromFile(filePath);
-
-        // Convert to adjacency list
-        HashMap<String,ArrayList<Edge>> adjList = Utilities.adjacencyListFromEdgeList(edgeList);
-
-        // Create graph with adjacency list
-        Graph graph = new Graph(adjList);
-
-        // Get all paths from startNode
-        HashMap<String,ArrayList<Edge>> paths = graph.allShortestPathsAdvanced(startNode);
-
-        // Return the longest path
-        return Utilities.findLongestPath(paths);
-
+        return "";
     }
 }
